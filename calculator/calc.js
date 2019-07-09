35df43f19c314e6bff79a89e3abda5d6bac6803d
Michał Białecki <miszekb@gmail.com>
2019-07-09 20:41:26 +0200

    still needs some debugging

--- calculator/calc.js
+++ calculator/calc.js
@@ -12,15 +12,16 @@ function generateButtons() {
     var content = "";
 
     for(i = 9; i >= 0; i--) {
-        content += '<span class="button" onclick="digitButtonClick('+i+')" id ="d_button'+ i +'">'+i+'</span>';
+        content += '<span class="dbutton" onclick="digitButtonClick('+i+')" id ="d_button'+ i +'">'+i+'</span>';
         if ((i-1) % 3 == 0) content += '<div style="clear: both;"></div>';
     }
     
     document.getElementById("digitButtons").innerHTML = content 
     + '<span class="button" onclick="addDot()" id ="d_button_dot">.</span>'  //coma
-    + '<span class="button" onclick="calculate() id ="d_button_dot">=</span>'  //equals
+    + '<span class="button" onclick="calculate()" id ="d_button_equals">=</span>'  //equals
     + '<div style="clear: both;"></div>'
-    + '<span class="button" onclick="clearWindow()" id ="d_button_dot">C</span>'; //clear
+    + '<span class="button" onclick="clearWindow()" id ="d_button_dot">C</span>' //clear
+    + '<span class="button" onclick="Backspace()" id ="d_button_dot"> <--- </span>';
     content = "";
 
     for(i = 0 ; i < operationsCodes.length; i++) {
@@ -40,7 +41,11 @@ function addDot() {
 
     result += ".";
     document.getElementById("result").innerHTML = result;
+}
 
+function Backspace() {
+    result = result.substr(0 , result.length - 1);
+    document.getElementById("result").innerHTML = result;
 }
 
 function clearWindow() {
@@ -49,6 +54,13 @@ function clearWindow() {
 }
 
 function digitButtonClick(id) {
+    if (result.length == 21) {
+        alert("Out of range :(");
+        return;
+    }
+
+    if(result.charAt(result.length - 1) == "0") return;
+
     for (i = 0; i < 10; i++) {
         if(id == i) {
             result += id;
@@ -59,6 +71,11 @@ function digitButtonClick(id) {
 
 function operationalButtonClick(id) {
 
+    if (result.length == 21) {
+        alert("Out of range :(");
+        return;
+    }
+
     if(result.charAt(result.length - 1) == "*" ||
     result.charAt(result.length - 1) == "+"||
     result.charAt(result.length - 1) == "-"||
@@ -67,7 +84,7 @@ function operationalButtonClick(id) {
     result.charAt(result.length - 1) == "."
     ) return;
 
-    for (i = 0 ; i < operationsCodes.length; i++) {
+    for (i = 0 ; i <= operationsCodes.length; i++) {
         if (id ==i) {
             result += operationsCodes.charAt(i);
             document.getElementById("result").innerHTML = result;
@@ -76,26 +93,106 @@ function operationalButtonClick(id) {
 }
 
 function calculate() {
-    for(i = 0; i < result.length ; i++) {
+    var values = new Array();
+    var currentOperations = new Array();
+    var cursor = 0;
+
+    console.log(result);
+    for(i = 0; i <= result.length ; i++) {
+        if(result.charAt(i) == "*" || result.charAt(i) == "/" ||
+        result.charAt(i) == "+" || result.charAt(i) == "-" ||
+        i == result.length) 
+        {
+            currentOperations.push(result.charAt(i));
+            values.push(parseFloat(result.substring(cursor, i)));
+            cursor = i+1;
+        }
         //TODO: determine an order of operations
         //TODO: separate those operations to an array in proper order
     }
+    currentOperations.pop(currentOperations.length);
+
+    var temp = 0;
+    var tempContainer = new Array();
+    var tempOperationsContainer = new Array();
+
+    for(i = 0; i < currentOperations.length; i++) {
+        if(currentOperations[i] == "*" || currentOperations[i] == "/") {
+            for(j = 0; j < values.slice(0,i).length; j++) {
+                tempContainer.push(values.slice(0,i)[j])
+            }
+            
+            if(currentOperations[i] == "*") {
+ 
+                temp = multiply(values[i], values[i+1]);
+                tempContainer.push(temp);
+            }
+            if(currentOperations[i] == "/") {
+                temp = divide(values[i], values[i+1]);
+                tempContainer.push(temp);
+            }
+
+            for(j = 0; j < values.slice(i+2, values.length).length; j++) {
+                tempContainer.push(values.slice(i+2, values.length)[j]);
+             }
+            
+             values = tempContainer;
+             console.log(values);
+             console.log(currentOperations);  
+
+             tempContainer = [];
+        }
+    }
+
+    for(i = 0; i < currentOperations.length; i++) {
+        if(currentOperations[i] == "+" || currentOperations[i] == "-") {
+            for(j = 0; j < values.slice(0,i).length; j++) {
+                tempContainer.push(values.slice(0,i)[j])
+            }
+            
+            if(currentOperations[i] == "*") {
+ 
+                temp = add(values[i], values[i+1]);
+                tempContainer.push(temp);
+            }
+            if(currentOperations[i] == "-") {
+                temp = subtract(values[i], values[i+1]);
+                tempContainer.push(temp);
+            }
+
+            for(j = 0; j < values.slice(i+2, values.length).length; j++) {
+                tempContainer.push(values.slice(i+2, values.length)[j]);
+             }
+            
+             values = tempContainer;
+             console.log(values);
+             console.log(currentOperations);  
+
+             tempContainer = [];
+        }
+    }
+    result = values[0].toString();
+    document.getElementById("result").innerHTML = result;
 }
 
 function add(param1, param2) {
     result = param1 + param2;
+    return result;
 }
 
 function subtract(param1, param2) {
     result = param1 - param2;
+    return result;
 }
 
 function divide(param1, param2) {
     result = param1 / param2;
+    return result;
 }
 
 function multiply(param1, param2) {
     result = param1 * param2;
+    return result;
 }
 
 function root(param1, param2) {
