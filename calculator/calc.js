9c59df00f31e7a75050417e34fecbe2dcf404372
Michał Białecki <miszekb@gmail.com>
2019-07-09 15:18:54 +0200

    cmon boi

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,6 +1,7 @@
 
-var result = 0;
+var result = "";
 var operationsCodes = "+-*/^";
+
 
 function showResult() {
     document.getElementById("result").innerHTML = result;
@@ -11,20 +12,74 @@ function generateButtons() {
     var content = "";
 
     for(i = 9; i >= 0; i--) {
-        content += '<span class="button" id ="d_button'+ i +'">'+i+'</span>';
+        content += '<span class="button" onclick="digitButtonClick('+i+')" id ="d_button'+ i +'">'+i+'</span>';
         if ((i-1) % 3 == 0) content += '<div style="clear: both;"></div>';
     }
     
-    document.getElementById("digitButtons").innerHTML = content + '<span class="button" id ="d_button_dot">.</span>';
-    
+    document.getElementById("digitButtons").innerHTML = content 
+    + '<span class="button" onclick="addDot()" id ="d_button_dot">.</span>'  //coma
+    + '<span class="button" onclick="calculate() id ="d_button_dot">=</span>'  //equals
+    + '<div style="clear: both;"></div>'
+    + '<span class="button" onclick="clearWindow()" id ="d_button_dot">C</span>'; //clear
     content = "";
 
     for(i = 0 ; i < operationsCodes.length; i++) {
-        content += '<span class="button" id ="o_button'+ operationsCodes.charAt(i) +'">'+ operationsCodes.charAt(i) +'</span>'+'<div style="clear: both;"></div>';
+        content += '<span class="button"  onclick="operationalButtonClick('+i+')" id ="o_button'
+        + i +'">'+ operationsCodes.charAt(i) 
+        +'</span>'+'<div style="clear: both;"></div>';
     }
-
     document.getElementById("operationalButtons").innerHTML = content;
 
+}
+
+function addDot() {
+    for(i = result.length - 1; i >= 0 ; i--) {
+        if(result.charAt(i) == ".") return;
+        //TODO: more than one dot
+    }
+
+    result += ".";
+    document.getElementById("result").innerHTML = result;
+
+}
+
+function clearWindow() {
+    result = "";
+    document.getElementById("result").innerHTML = result;
+}
+
+function digitButtonClick(id) {
+    for (i = 0; i < 10; i++) {
+        if(id == i) {
+            result += id;
+            document.getElementById("result").innerHTML = result;
+        }
+    }
+}
+
+function operationalButtonClick(id) {
+
+    if(result.charAt(result.length - 1) == "*" ||
+    result.charAt(result.length - 1) == "+"||
+    result.charAt(result.length - 1) == "-"||
+    result.charAt(result.length - 1) == "/"||
+    result.charAt(result.length - 1) == "^"||
+    result.charAt(result.length - 1) == "."
+    ) return;
+
+    for (i = 0 ; i < operationsCodes.length; i++) {
+        if (id ==i) {
+            result += operationsCodes.charAt(i);
+            document.getElementById("result").innerHTML = result;
+        }
+    }
+}
+
+function calculate() {
+    for(i = 0; i < result.length ; i++) {
+        //TODO: determine an order of operations
+        //TODO: separate those operations to an array in proper order
+    }
 }
 
 function add(param1, param2) {
